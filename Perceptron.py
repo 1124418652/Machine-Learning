62ab29d6303015e785e4b3d235e58287cd3e4029
hejun <1124418652@qq.com>
2018-07-25 15:58:39 +0800

    finish the function dual_perceptron()

--- Perceptron.py
+++ Perceptron.py
@@ -65,20 +65,46 @@ class Perceptron(object):
 		input_array, label = self.segmentation()
 		input_array = np.array(input_array)
 		a = np.zeros(len(input_array))         # array a was used to storage the frequency of point i 
+		w = np.zeros(len(input_array[0]))
 		b = 0
+		num = 0
 
 		gram = np.zeros((len(a), len(a)))      # gram matrix
 		for index1, value1 in enumerate(input_array):
 			for index2, value2 in enumerate(input_array):
 				gram[index1][index2] = (value1*value2).sum()
 
-		print(gram)
+		error_list = set(range(0, len(label)))
+
+		while(error_list):
+			for index, value in enumerate(input_array):
+				num += 1
+				condition  = ((a*label*gram[index]).sum() + b) * label[index]
+				
+				if(condition<=0):
+					a[index] += 1
+					b = b + step*label[index]
+					error_list.add(index)
+
+				elif(condition>0 and index in error_list):
+					error_list.remove(index)
+
+		for index, value in enumerate(a):
+			w += value * input_array[index] * label[index]
+		
+		end_time = time.time()
+		print("Running time of function \"norm_perceptron\": %f s" %(end_time-start_time))
+		print("The frequency of training: %d" %(num))
+		print("The final model: %s*x1 + %s*x2 + %s\n" %(w[0], w[1], b))	
 
 def main():
 	data = [[3, 3, 1],
 			[4, 3, 1],
 			[1, 1, -1],
-			[0, 2, -1]]
+			[0, 0, -1],
+			[2, 0, -1],
+			[0, 2 , -1]]
+
 	percept = Perceptron(data)
 	percept.norm_perceptron()
 	percept.dual_perceptron()
