09c72c56e5045518d5232aee96b6568b405a1f0e
徐何君 <1124418652@qq.com>
2018-09-24 16:24:11 +0800

    create the class logistic

--- logistics/logistic_regression.py
+++ logistics/logistic_regression.py
@@ -1,68 +1,43 @@
 # -*- coding: utf-8 -*-
 """
 # project: Logistic regression
-# author: xhj
-# email: 1124418652@qq.com
-# date: 2018 9/7
+# author:  xhj
+# email:   1124418652@qq.com
+# date:    2018 9/7
 """
 
+import os
+import sys
+import csv
 import time
 import numpy as np
 from math import exp, log
+import matplotlib.pyplot as plt
 
-def seperate_data(data):
-    label = []
-    x = []
+class logistic(object):
+    def __init__(self, alpha = 0.1):
+        self._alpha = alpha
 
-    for line in data:
-        x.append(line[:-1])
-        label.append(line[-1])
+    def load_data(self, path):
+        if False == os.path.isfile(path):
+            print("Don't find this file!")
+            exit()
 
-    x = np.array(x)
-    return x, label
+        data = []
+        label = []
+        with open(path, "r") as fr:
+            reader = csv.reader(fr)
+            for row in reader:
+                data.append(row[: -1])
+                label.append(row[-1])
 
-def model_training(data_set, label, a = 1, w = None, b = 0, iterate = 400):
-    """
-    logistic model: e^(w*x+b)/[1+e^(w*x+b)]
-    cost function:  -[y*log(yi) + (1-y)*log(1-yi)]
-    temp variables:
-        z = w.T * x
-        a = e^z / (1 + e^z)
-    """
-    num = len(data_set)
-    dims = len(data_set[0])
-    # w = np.zeros(dims, float)
-    error = 0.0
-    i = 0
-    w = np.zeros(dims, float)
-    b = 0.0
-
-    while(i < iterate):
-        index = 0
-
-        for line in data_set:
-            z = (w * line).sum() + b
-            a = exp(z) / (1+exp(z))
-            # error -= (label[index] * log(a) + (1 - label[index]) * log(1 - a))
-            w += (label[index] - a) * line
-            b += label[index] - a
-
-            index += 1
-        i += 1
-    return w, b
+        return np.array(data), np.array(label)
 
 def main():
-    data = [[3, 3, 1],
-            [4, 3, 1],
-            [1, 1, 0],
-            [0, 0, 0],
-            [2, 0, 0],
-            [0, 2, 0]]
-    data = np.array(data)
-    x, label = seperate_data(data)
-    w, b = model_training(x, label)
-    print(w, b)
-    print(1 / (1 + exp((w*np.array([0,2])).sum())))
+    path = "train.txt"
+    logis = logistic()
+    data, label = logis.load_data(path)
+    print(data, label)
 
 if __name__ == '__main__':
     main()
